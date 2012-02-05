4da907d60845141b9a760bd1aba36ff4bc3d7231
postite <blog@postite.com>
2012-02-05 02:53:53 +0100

    Dictionary > ObjectHash
    object hash could be intHash

--- com/citrusengine/core/Input.hx
+++ com/citrusengine/core/Input.hx
@@ -3,21 +3,22 @@ package com.citrusengine.core;
 import flash.events.KeyboardEvent;
 //PORTODO DICTIONARY
 import flash.utils.Dictionary;
+import com.citrusengine.utils.ObjectHash;
 
 class Input {
 	public var enabled(getEnabled, setEnabled) : Bool;
 
-	static public var JUST_PRESSED : UInt = 0;
-	static public var DOWN : UInt = 1;
-	static public var JUST_RELEASED : UInt = 2;
-	static public var UP : UInt = 3;
-	var _keys : Dictionary;
+	static public var JUST_PRESSED : Int = 0;
+	static public var DOWN : Int = 1;
+	static public var JUST_RELEASED : Int = 2;
+	static public var UP :Int = 3;
+	var _keys : ObjectHash<Int>;
 	var _keysReleased : Array<Int>;
 	var _initialized : Bool;
 	var _enabled : Bool;
 	public function new() {
 		_enabled = true;
-		_keys = new Dictionary();
+		_keys = new ObjectHash<Int>();
 		_keysReleased = new Array<Int>();
 	}
 
@@ -30,7 +31,7 @@ class Input {
 	}
 
 	public function setEnabled(value : Bool) : Bool {
-		if(_enabled == value) return;
+		if(_enabled == value) return value;
 		_enabled = value;
 		var ce : CitrusEngine = CitrusEngine.getInstance();
 		if(_enabled)  {
@@ -52,11 +53,13 @@ class Input {
 	 */
 	public function update() : Void {
 		if(!_enabled) return;
-		for(key in Reflect.fields(_keys)) {
-			if(Reflect.field(_keys,key) == JUST_PRESSED) Reflect.setField(_keys,key , DOWN);
+		for(key in _keys) {
+			if( _keys.get(key)==JUST_PRESSED)
+			_keys.set(key,DOWN);
+			
 		}
 
-		_keysReleased.length = 0;
+		_keysReleased = [];
 	}
 
 	/**
@@ -75,7 +78,8 @@ class Input {
 	 * @param keyCode a "code" representing a key on the keyboard. Use Flash's Keyboard class constants if you please. 
 	 */
 	public function isDown(keyCode : Int) : Bool {
-		return _keys[keyCode] == DOWN;
+		//return _keys[keyCode] == DOWN;
+		return _keys.get(keyCode)==DOWN;
 	}
 
 	/**
@@ -83,7 +87,8 @@ class Input {
 	 * @param keyCode a "code" representing a key on the keyboard. Use Flash's Keyboard class constants if you please. 
 	 */
 	public function justPressed(keyCode : Int) : Bool {
-		return _keys[keyCode] == JUST_PRESSED;
+		//return _keys[keyCode] == JUST_PRESSED;
+		return _keys.get(keyCode)==JUST_PRESSED;
 	}
 
 	/**
@@ -91,26 +96,33 @@ class Input {
 	 * @param keyCode a "code" representing a key on the keyboard. Use Flash's Keyboard class constants if you please. 
 	 */
 	public function justReleased(keyCode : Int) : Bool {
-		return _keysReleased.indexOf(keyCode) != -1;
+		//return _keysReleased.indexOf(keyCode) != -1;
+		return Lambda.indexOf(_keysReleased,keyCode) !=-1;
 	}
 
 	/**
 	 * Returns an unsigned integer representing the key's current state.
 	 * @param keyCode a "code" representing a key on the keyboard. Use Flash's Keyboard class constants if you please. 
 	 */
-	public function getState(keyCode : Int) : UInt {
-		if(_keys[keyCode]) return _keys[keyCode]
-		else if(_keysReleased.indexOf(keyCode) != -1) return JUST_RELEASED
+	public function getState(keyCode : Int) : Int {
+		if( _keys.get(keyCode)!=Math.NaN) return _keys.get(keyCode);
+		//if(_keys[keyCode]) return _keys[keyCode]
+		else if (Lambda.indexOf(_keysReleased,keyCode)!=-1 )return JUST_RELEASED;
+		//else if(_keysReleased.indexOf(keyCode) != -1) return JUST_RELEASED
 		else return UP;
+		
 	}
 
 	function onKeyDown(e : KeyboardEvent) : Void {
-		if(!_keys[e.keyCode]) _keys[e.keyCode] = JUST_PRESSED;
+		if( _keys.get(e.keyCode)!=Math.NaN)
+		_keys.set(e.keyCode,JUST_PRESSED);
+		//if(!_keys[e.keyCode]) _keys[e.keyCode] = JUST_PRESSED;
 	}
 
 	function onKeyUp(e : KeyboardEvent) : Void {
 
-		delete	_keys[e.keyCode];
+		_keys.remove(e.keyCode);
+		//delete	_keys[e.keyCode];
 
 		_keysReleased.push(e.keyCode);
 	}
